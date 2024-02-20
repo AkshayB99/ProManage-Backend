3b749e3a2848fc77dcf7f3c4015abfa62fbef1e5
AkshayB99 <akshaybharamshetti99@gmail.com>
2024-02-20 21:29:40 +0530

    User Auth Done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const AppError = require("./utils/appError");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRouter");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
+
+app.use(cookieParser());
 app.use(express.json());
 
 // Router
