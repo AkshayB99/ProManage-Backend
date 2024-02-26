372cc6495a3f8f8aa5166451b91d3f6257544664
AkshayB99 <akshaybharamshetti99@gmail.com>
2024-02-26 19:27:22 +0530

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
